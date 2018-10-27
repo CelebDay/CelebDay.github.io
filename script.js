90ad2be4cf31414dac2a57a775b9fdcafdf4f35f
HiteshBday2018 <apoorvakshri@gmail.com>
2018-10-27 17:56:06 +0530

    audiochanges

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
@@ -65,3 +68,29 @@ function fade_in() {
 
 // automatic popup
 
+jQuery(document).ready(function($){
+  
+  window.onload = function (){
+    $(".bts-popup").delay(1000).addClass('is-visible');
+    }
+  
+    //open popup
+    $('.bts-popup-trigger').on('click', function(event){
+        event.preventDefault();
+        $('.bts-popup').addClass('is-visible');
+    });
+    
+    //close popup
+    $('.bts-popup').on('click', function(event){
+        if( $(event.target).is('.bts-popup-close') || $(event.target).is('.bts-popup') ) {
+            event.preventDefault();
+            $(this).removeClass('is-visible');
+        }
+    });
+    //close popup when clicking the esc keyboard button
+    $(document).keyup(function(event){
+        if(event.which=='27'){
+            $('.bts-popup').removeClass('is-visible');
+        }
+    });
+});
